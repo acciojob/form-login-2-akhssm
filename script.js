b357413e1a3e6ad967dafce3b7141fb21368122f
Akshay <152252912+akhssm@users.noreply.github.com>
2025-11-03 11:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", function() {
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const phone = document.getElementById("phoneNumber").value;
+  const email = document.getElementById("emailID").value;
+
+  alert(
+    `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`
+  );
+});
